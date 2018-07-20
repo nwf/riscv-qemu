8c815d84119009f3af2a1e892cb3f975bc9a4005
Palmer Dabbelt <palmer@sifive.com>
2018-07-19 18:25:16 -0700

    sifive_prci: Emulate the HFXOSC configuration register
    I figured that when I went to touch the PRCI I might as well read the
    documentation and make it match the HiFive1.  This patch adds the
    external oscillator control register, which reads as the most likely
    value (it's enabled on reset, and it's probably locked by the time
    you're trying to read it).
    
    Like the others, I haven't compiled this.
    
    Signed-off-by: Palmer Dabbelt <palmer@sifive.com>

--- hw/riscv/sifive_prci.c
+++ hw/riscv/sifive_prci.c
@@ -30,6 +30,9 @@ static uint64_t sifive_prci_read(void *opaque, hwaddr addr, unsigned int size)
     if (addr == 0 /* PRCI_HFROSCCFG    */) {
         return 1 << 31; /* ROSC_RDY    */
     }
+    if (addr == 4 /* PRCI_HFXOSCCFG    */) {
+        return 3 << 30; /* XOSC_RDY,EN */
+    }
     if (addr == 8 /* PRCI_PLLCFG       */) {
         return 1 << 31; /* PLL_LOCK    */
     }
